c60aff6ea9bde0e8a205e08308fac1a80203f0fc
susheel kumar <techiesusheel@gmail.com>
2025-08-22 15:54:34 +0530

    day 1 php up-1

---
+++ php/day-1_php/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>day-1_php</title>
+</head>
+ <style>
+    b{
+        color:chocolate
+    }
+ </style>
+<body>
+    <hr>
+    <?php
+
+    $name = "susheel";
+    $fullname = "susheel kumar";
+    $sitename = "susheel_coder";
+    $DOB = "29 April 2003";
+    $passion = "web design";
+
+    echo "Hi, my name is <b>$name</b>.<br>";
+    echo "My full name is <b>$fullname</b>  and I am a web developer.<br>";
+    echo "I am the creator of the website <b>$sitename</b>.<br>";
+    echo "I love coding, learning new technologies, and creating unique projects.<br>";
+    echo "My date of birth is <b>$DOB</b>, and I am passionate about <b>$passion</b>.<br>";
+
+
+   echo "<br><br><br><hr>";
+    echo "Hi, my name is <b>$name</b>.<br>
+     My full name is <b>$fullname</b>  and I am a web developer.<br>
+     I am the creator of the website <b>$sitename</b>.<br>
+     I love coding, learning new technologies, and creating unique projects.<br>
+     My date of birth is <b>$DOB</b>, and I am passionate about <b>$passion</b>.<br>";
+    ?>
+</body>
+</html>
