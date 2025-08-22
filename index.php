37d643a3f3c80d9e12deed8f132e0ef51bbdb952
susheel kumar <techiesusheel@gmail.com>
2025-08-22 16:14:21 +0530

    up-3

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php </title>
+</head>
+
+<body>
+    <h1>PhP folder </h1>
+    <h2>
+        <ul>
+            <li> <a href="/php/day-1_php/index.php">day-1_php</a></li>
+            <!-- <li> <a href="/php/day-2_php/index.php">day-2_php</a></li> -->
+        </ul>
+    </h2>
+</body>
+
+</html>
