27d851a6075c684c9154a31ac864340439a37f86
susheel kumar <techiesusheel@gmail.com>
2025-08-28 03:14:33 +0530

    Create index.php

---
+++ php/day-11_php/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php</title>
+</head>
+<style>
+    body{
+        margin: 50px;
+    }
+    .section{
+        background-color: antiquewhite;
+        padding: 10px;
+    }
+</style>
+<body>
+    <section class="section">
+        <hr>
+    <form action="skilles.php" method="post" enctype="multipart/form-data">
+        <input type="text" name="name" placeholder="Enter your name" ><br><br>
+        <input type="text" name="skills1" placeholder="skills-1" ><br><br>
+        <input type="text" name="skills2" placeholder="skills-2" ><br><br>
+        <input type="text" name="skills3" placeholder="skills-3" ><br><br>
+        <input type="text" name="skills4" placeholder="skills-4" ><br><br>
+        <input type="text" name="skills5" placeholder="skills-5" ><br><br>
+        <input type="text" name="skills6" placeholder="skills-6" ><br><br>
+        <input type="text" name="skills7" placeholder="skills-7" ><br><br>
+        <input type="text" name="skills8" placeholder="skills-8" ><br><br>
+        <input type="text" name="skills9" placeholder="skills-9" ><br><br>
+        <input type="text" name="skills10" placeholder="skills-10" ><br><br>
+        <!-- <input type="text" name="skills4" placeholder="skills-4" ><br><br> -->
+
+        <!-- <input type="checkbox" name="skills1" value="html" >html
+        <input type="checkbox" name="skills1" value="css"  >css
+        <input type="checkbox" name="skills1" value="js" >js
+        <input type="checkbox" name="skills1" value="python"  >python <br>
+        <input type="checkbox" name="skills2" value="html" >html
+        <input type="checkbox" name="skills2" value="css"  >css
+        <input type="checkbox" name="skills2" value="js" >js
+        <input type="checkbox" name="skills2" value="python"  >python  <br>
+        <input type="checkbox" name="skills3" value="html" >html
+        <input type="checkbox" name="skills3" value="css"  >css
+        <input type="checkbox" name="skills3" value="js" >js
+        <input type="checkbox" name="skills3" value="python"  >python  <br> -->
+        <input type="checkbox" name="skills4" value="React"  >React  <br> 
+        <!-- <input type="file" name="image" required><br><br> -->
+        <input type="submit" name="submit" value="Upload">
+    </form>
+    <hr>
+  </section>
+ 
+    
+</body>
+</html>
